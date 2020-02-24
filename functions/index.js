9a2c8ea4eb795522640d6800e1d3a32ee57fa028
Mudit Jain <ciphereck@gmail.com>
2020-02-24 12:15:12 +0530

    adding firebase basic code after first deployment
    Signed-off-by: Mudit Jain <ciphereck@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,26 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.use('/scxaaaa', (req, res) => {
+
+    res.send("saaasasasasds");
+})
+
+app.use('/developers', (req, res) => {
+	res.status(200).json({
+		"developers": ["Mudit", "Navjot", "Ekta"]
+	})
+})
+
+exports.api = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
