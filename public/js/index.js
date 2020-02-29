904c966a68684876579faad963083c35b381f138
Mudit Jain <ciphereck@gmail.com>
2020-02-29 15:44:12 +0530

    adding voter detail page
    Signed-off-by: Mudit Jain <ciphereck@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,9 @@ console.log(config.BASE)
 
 
 
-function getDevelopers() {
+function getDevelopers(id) {
 	console.log("developers")
-	const req = axios.get(config.BASE + "/voter/info?aadhaarNo=7206307664")	
+	const req = axios.get(config.BASE + "/voter/info?aadhaarNo=" + id)	
 	return req.then(token => {
 		console.log(token.data)
 		return token.data
