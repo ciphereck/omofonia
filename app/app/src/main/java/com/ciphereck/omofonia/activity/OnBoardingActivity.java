f6ebed46605304520c33eab2b23a76b680728b33
Mudit Jain <ciphereck@gmail.com>
2020-05-21 13:37:35 +0530

    Adding Onboarding activity
    Signed-off-by: Mudit Jain <ciphereck@gmail.com>

--- app/app/src/main/java/com/ciphereck/omofonia/activity/OnBoardingActivity.java
+++ app/app/src/main/java/com/ciphereck/omofonia/activity/OnBoardingActivity.java
@@ -3,14 +3,37 @@ package com.ciphereck.omofonia.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ciphereck.omofonia.R;
 
+
 public class OnBoardingActivity extends AppCompatActivity {
+    Button submitButton;
+    EditText editText;
+    EditText editText1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        initView();
+    }
+
+    private void initView() {
+        submitButton = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        editText1 = findViewById(R.id.editText2);
+
+        findViewById(R.id.button).setOnClickListener(v -> {
+            String aadhaarData = editText.getText().toString();
+            String password = editText1.getText().toString();
+            System.out.println(aadhaarData);
+            System.out.println(password);
+        });
     }
 }
